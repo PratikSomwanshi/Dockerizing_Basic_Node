88bcf08f9c195b1aa9e3cfbd80ad84093ac06281
Pratik Somwanshi <pratiksomwanshi570@gmail.com>
2024-02-03 19:07:05 +0530

    basic docker setup

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const app = express()
+
+
+app.get("/", (req,res) => {
+    res.json({message: "Hello World!"})
+})
+
+app.listen(3000 , () => {
+    console.log("server running on 3000")
+})
